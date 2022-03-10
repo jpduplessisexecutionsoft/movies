0fb6af6256493393a8aa408df2e216ccf19a91a4
JP Du Plessis <jp@executionsoft.com>
2022-03-10 21:32:55 +0200

    acl functional.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\TheaterController;
+use app\Http\Controllers\MovieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', function () {
+    return view('welcome');
+});
+
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('Theater', TheaterController::class);
+    Route::resource('Movie', MovieController::class);
 });
